dbd3bee43a81c9dae61f1a160143084e128e0a78
James Fairweather <james.fairweather@blackbirdinteractive.com>
2022-04-22 12:15:14 -0700

    Add new test suite for HackerRank tests

---
+++ HackerRank/HackerRank.h
@@ -0,0 +1,19 @@
+
+#include <algorithm>
+#include <cmath>
+#include <cstdio>
+#include <sstream>
+#include <iostream>
+#include <memory>
+#include <map>
+#include <unordered_map>
+#include <vector>
+
+#include "pch.h"
+
+namespace HackerRank {
+	class Solution {
+	public:
+		int					candies();
+	};
+}
---
+++ TestCases/HackerRank.cpp
@@ -0,0 +1,9 @@
+#include "pch.h"
+
+#include "gtest/gtest.h"
+
+#include "../HackerRank/HackerRank.h"
+
+TEST(TestCase_HackerRank_Candies, Test1) {
+	//HackerRank::Solution sln;
+}
